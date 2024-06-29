74cca8d3a270e879c6dc44e11d789476d50ba676
nuagenic <nuagenic@gmail.com>
2024-06-29 21:22:45 +0900

    feat: Add typescript, lint, prettier settings

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+};
+
+export default App;
