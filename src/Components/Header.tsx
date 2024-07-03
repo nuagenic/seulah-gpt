332ad1e9940f612a92678fdd55dfe7f6bd263fbd
nuagenic <nuagenic@gmail.com>
2024-07-04 01:45:50 +0900

    feat:add close png

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,13 @@
 const Header = () => {
   return (
-    <div className="mb-10 text-4xl flex gap-8 animate-blink items-baseline">
-      <h1 className="text-white font-seula leading-none">SEULA</h1>
-      <h1 className="text-white font-basic tracking-widest leading-none">
-        v1.0.0
-      </h1>
+    <div className="mb-10 text-4xl flex justify-between">
+      <div className="flex gap-8 animate-blink items-baseline">
+        <h1 className="text-white font-seula leading-none">SEULA</h1>
+        <h1 className="text-white font-basic tracking-widest leading-none">
+          v1.0.0
+        </h1>
+      </div>
+      <img className="w-8 h-8 self-center" src="/close.png" alt="" />
     </div>
   )
 }
