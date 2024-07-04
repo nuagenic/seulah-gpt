71824524b516c5c5527783a3bb3afc9c5eed7ebc
nuagenic <nuagenic@gmail.com>
2024-07-04 15:40:49 +0900

    fix: Fix Line

--- src/Components/Line.tsx
+++ src/Components/Line.tsx
@@ -3,11 +3,12 @@ export default function Line() {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       height="2"
-      viewBox="0 0 1520 2"
+      viewBox="0 0 100 2"
       fill="none"
       className="w-full stroke-2"
+      preserveAspectRatio="none"
     >
-      <path d="M0 1.33325H1520" stroke="white" />
+      <path d="M0 1H100" stroke="white" />
     </svg>
   )
 }
